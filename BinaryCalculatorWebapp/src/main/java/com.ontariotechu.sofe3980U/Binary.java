a9802b569e6328f00dbd0d0aa97e6fd1c134c0fc
Leona <leonakrasniqi2005@gmail.com>
2026-03-14 02:25:22 -0400

    Update Binary.java

--- BinaryCalculatorWebapp/src/main/java/com.ontariotechu.sofe3980U/Binary.java
+++ BinaryCalculatorWebapp/src/main/java/com.ontariotechu.sofe3980U/Binary.java
@@ -1,82 +1,148 @@
 package com.ontariotechu.sofe3980U;
 
-/**
- * Unsigned integer Binary variable
- *
- */
 public class Binary
 {
-	private String number="0";  // string containing the binary value '0' or '1'
-	/**
-	* A constructor that generates a binary object.
-	*
-	* @param number a String of the binary values. It should conatins only zeros or ones with any length and order. otherwise, the value of "0" will be stored.   Trailing zeros will be excluded and empty string will be considered as zero.
-	*/
+    private String number = "0";  
+
+    /**
+     * @param number 
+     */
     public Binary(String number) {
-		for (int i = 0; i < number.length(); i++) {
-			// check each character if it's not 0 or 1
-			char ch=number.charAt(i);
-			if(ch!='0' && ch!='1') {
-				number="0"; // if not store "0" and end the function
-				return;
-			}
-		}
-		// remove any trailing zeros
-		int beg;
-		for (beg = 0; beg < number.length(); beg++) {
-			if (number.charAt(beg)!='0')
-				break;
-		}
-		//beg has the index of the first non zero digit in the number
-		this.number=number.substring(beg); // exclude the trailing zeros if any
-		// uncomment the following code
-		
-		if(this.number=="") { // replace empty strings with a single zero
-			this.number="0";
-		}
-		
+        if (number == null || number.isEmpty()) {
+            this.number = "0";
+            return;
+        }
+
+    
+        for (int i = 0; i < number.length(); i++) {
+            char ch = number.charAt(i);
+            if (ch != '0' && ch != '1') {
+                this.number = "0";
+                return;
+            }
+        }
+
+    
+        int beg;
+        for (beg = 0; beg < number.length(); beg++) {
+            if (number.charAt(beg) != '0') {
+                break;
+            }
+        }
+
+    
+        this.number = (beg == number.length()) ? "0" : number.substring(beg);
+
+
     }
-	/**
-	* Return the binary value of the variable
-	*
-	* @return the binary value in a string format.
-	*/
-	public String getValue()
-	{
-		return this.number;
-	}
-	/**
-	* Adding two binary variables. For more information, visit <a href="https://www.wikihow.com/Add-Binary-Numbers"> Add-Binary-Numbers </a>.
-	*
-	* @param num1 The first addend object
-	* @param num2 The second addend object
-	* @return A binary variable with a value of <i>num1+num2</i>.
-	*/
-	public static Binary add(Binary num1,Binary num2)
-	{
-		// the index of the first digit of each number
-		int ind1=num1.number.length()-1;
-		int ind2=num2.number.length()-1;
-		//initial variable
-		int carry=0;
-		String num3="";  // the binary value of the sum
-		while(ind1>=0 ||  ind2>=0 || carry!=0) // loop until all digits are processed
-		{
-			int sum=carry; // previous carry
-			if(ind1>=0){ // if num1 has a digit to add
-				sum += (num1.number.charAt(ind1)=='1')? 1:0; // convert the digit to int and add it to sum
-				ind1--; // update ind1
-			}
-			if(ind2>=0){ // if num2 has a digit to add
-				sum += (num2.number.charAt(ind2)=='1')? 1:0; // convert the digit to int and add it to sum
-				ind2--; //update ind2
-			}
-			carry=sum/2; // the new carry
-			sum=sum%2;  // the resultant digit
-			num3 =( (sum==0)? "0":"1")+num3; //convert sum to string and append it to num3
-		}
-		Binary result=new Binary(num3);  // create a binary object with the calculated value.
-		return result;
-		
-	}
-}	
+
+    /**
+
+     * @return the binary value in a string format.
+     */
+    public String getValue()
+    {
+        return this.number;
+    }
+
+    /**
+     * @param num1 
+     * @param num2 
+     * @return 
+     */
+    public static Binary add(Binary num1, Binary num2)
+    {
+        int ind1 = num1.number.length() - 1;
+        int ind2 = num2.number.length() - 1;
+
+        int carry = 0;
+        String num3 = "";
+
+        while (ind1 >= 0 || ind2 >= 0 || carry != 0)
+        {
+            int sum = carry;
+
+            if (ind1 >= 0) {
+                sum += (num1.number.charAt(ind1) == '1') ? 1 : 0;
+                ind1--;
+            }
+
+            if (ind2 >= 0) {
+                sum += (num2.number.charAt(ind2) == '1') ? 1 : 0;
+                ind2--;
+            }
+
+            carry = sum / 2;
+            sum = sum % 2;
+
+            num3 = ((sum == 0) ? "0" : "1") + num3;
+        }
+
+        return new Binary(num3);
+    }
+
+    
+    public static Binary or(Binary num1, Binary num2)
+    {
+        int i = num1.number.length() - 1;
+        int j = num2.number.length() - 1;
+        String result = "";
+
+        while (i >= 0 || j >= 0)
+        {
+            char a = (i >= 0) ? num1.number.charAt(i) : '0';
+            char b = (j >= 0) ? num2.number.charAt(j) : '0';
+
+            result = ((a == '1' || b == '1') ? "1" : "0") + result;
+
+            i--;
+            j--;
+        }
+
+        return new Binary(result);
+    }
+
+    
+    public static Binary and(Binary num1, Binary num2)
+    {
+        int i = num1.number.length() - 1;
+        int j = num2.number.length() - 1;
+        String result = "";
+
+        while (i >= 0 || j >= 0)
+        {
+            char a = (i >= 0) ? num1.number.charAt(i) : '0';
+            char b = (j >= 0) ? num2.number.charAt(j) : '0';
+
+            result = ((a == '1' && b == '1') ? "1" : "0") + result;
+
+            i--;
+            j--;
+        }
+
+        return new Binary(result);
+    }
+
+    public static Binary multiply(Binary num1, Binary num2)
+    {
+        Binary result = new Binary("0");
+
+        int shift = 0;
+        for (int i = num2.number.length() - 1; i >= 0; i--)
+        {
+            if (num2.number.charAt(i) == '1')
+            {
+        
+                String shifted = num1.number;
+                for (int k = 0; k < shift; k++) {
+                    shifted = shifted + "0";
+                }
+
+                result = Binary.add(result, new Binary(shifted));
+            }
+            shift++;
+        }
+
+        return result;
+    }
+}
